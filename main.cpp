ba9e4ffc3b2139c47061640df0f096c3ee194516
Eisa <ealkaabi1@mail.csuchico.edu>
2017-10-04 20:18:02 -0700

    main.cpp with comments

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// include the bitmap header
+// other header files that may need
+
+int main()
+{
+  // create bitmap object
+
+  // ask the user for a valid file name
+  // if the user enter not a valid file name then give the error message
+  // repeat this process until a valid file name is entered and the file is 
+  // valid 24 bit depth windows bmp file
+
+  // when valid file is enter then loop through each pixel and average
+  // the rgb values and then assign all of the rgb values the resulting
+  // average to make the grayscale
+
+  // save the file to oldtimey.bmp
+
+  return 0;
+}
