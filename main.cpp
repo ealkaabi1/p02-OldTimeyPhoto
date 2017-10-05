416adeb78f9c2a5bf069b3c7c5d9b6c65b98795c
Eisa <ealkaabi1@mail.csuchico.edu>
2017-10-04 20:30:58 -0700

    Opens a picture and converts to pixel matrix

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
-// include the bitmap header
-// other header files that may need
+#include <vector>
+#include <iostream>
+#include "bitmap.h"
+
+using namespace std;
 
 int main()
 {
-  // create bitmap object
+  Bitmap image;
+  vector <vector <Pixel> > bmp;
+  Pixel rgb;
+  string file = "machupicchu.bmp";
 
-  // ask the user for a valid file name
-  // if the user enter not a valid file name then give the error message
-  // repeat this process until a valid file name is entered and the file is 
-  // valid 24 bit depth windows bmp file
+  image.open(file);
+  bmp = image.toPixelMatrix();
 
-  // when valid file is enter then loop through each pixel and average
-  // the rgb values and then assign all of the rgb values the resulting
-  // average to make the grayscale
-
-  // save the file to oldtimey.bmp
+  cout << file << " has been loaded. It is " << bmp[0].size() << " pixels "
+    << "wide and " << bmp.size() << " high" << endl;
 
   return 0;
 }
