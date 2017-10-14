edfb90aca0279a9803b51241c3f5d49c330e7658
ealkaabi1 <ealkaabi1@mail.csuchico.edu>
2017-10-14 14:51:25 -0700

    Finish most of main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+//Eisa Alkaabi
+//CSCI 111
+//p02-OldTimeyPhoto
+
 #include <vector>
 #include <iostream>
 #include "bitmap.h"
@@ -8,14 +12,44 @@ int main()
 {
   Bitmap image;
   vector <vector <Pixel> > bmp;
-  Pixel rgb;
-  string file = "machupicchu.bmp";
+  string file;
 
-  image.open(file);
+  do
+  {
+    cout << "Filename: ";
+    getline(cin, file);
+    image.open(file);
+
+    if (image.isImage() == false)
+    {
+      cout << "\nThe file must be a valid 24 bit depth Windows BMP image file." << endl;
+      cout << "Please try another filename.\n" << endl;
+    }
+  } while (image.isImage() == false);
+
   bmp = image.toPixelMatrix();
 
   cout << file << " has been loaded. It is " << bmp[0].size() << " pixels "
     << "wide and " << bmp.size() << " high" << endl;
+
+  cout << "Converting to grayscale........" << endl;
+
+  for (int i = 0; i < bmp.size(); i++)
+  {
+    for (int j = 0; j < bmp[i].size(); j++)
+    {
+      // still need work this out
+    }
+  }
+
+
+  // overwrite the current bitmap with the pixel matrix
+  image.fromPixelMatrix(bmp);
+
+  // save the file as oldtimey.bmp
+  image.save("oldtimey.bmp");
+
+  cout << "Done." << endl;
 
   return 0;
 }
